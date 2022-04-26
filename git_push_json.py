cba5956fd24558f958a1e9160aae1b124a96df17
CGLee <cglee@snowpipe.co.kr>
2022-04-26 14:38:40 +0900

    Refactor: 기획데이터 Json파일로 저장

--- git_push_json.py
+++ git_push_json.py
@@ -1,23 +1,22 @@
 import logging
-import git
+from git import Repo
 from pathlib import Path
 
-import self as self
-from git import Repo
+
 PATH_FOR_DATA = Path("./data-for-designer")
 PATH_FOR_GIT = Path("./data-for-designer/.git")
+REPO_FOR_DATA =  "http://172.20.41.70:3000/SPTeam/data-for-designer.git"
 
 def git_init():
     try:
-        repo = git.Repo.clone_from('http://172.20.41.70:3000/SPTeam/data-for-designer.git',
-                                   './data-for-designer',branch='main')
+        repo = Repo.clone_from(REPO_FOR_DATA, './data-for-designer', branch='main')
     except Exception as e:
         logging.warning('GIT Push 에러 발생' + e)
 
 
 def git_push():
     try:
-        repo = git.Repo(PATH_FOR_GIT)
+        repo = Repo(PATH_FOR_GIT)
         repo.git.checkout('main')
         origin = repo.remotes.origin
         origin.pull()
