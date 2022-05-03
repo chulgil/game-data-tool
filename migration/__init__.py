bdb619fda1ad47b54a7d5ca6f23c1efa1493ac94
CGLee <cglee@snowpipe.co.kr>
2022-05-03 16:05:42 +0900

    Chore: 프로젝트 환경설정

--- migration/__init__.py
+++ migration/__init__.py
@@ -1,5 +1,3 @@
 # from .git_manager import GitManager
 # from .data_manager import DataManager
 # from .db_manager import DBManager
-from app.libs.excel_to_db.migration.prisma_manager import PrismaManager
-PrismaManager('local')
