75a277ea9a7a90fc231e5aaae061c91517d61cf0
CGLee <cglee@snowpipe.co.kr>
2022-05-05 22:12:07 +0900

    Fix: Git checkout 수정

--- migration/git_manager.py
+++ migration/git_manager.py
@@ -33,6 +33,7 @@ class GitManager:
 
     def _init_git(self):
         try:
+            self._repo = Repo(self.PATH_FOR_GIT)
             # GIT 기본 프로젝트 폴더 삭제
             if Path(self.BRANCH).is_dir():
                 shutil.rmtree(self.BRANCH)
