323e336c2412d42e70761e4831427999a2d0b23b
CGLee <cglee@snowpipe.co.kr>
2022-07-07 20:11:34 +0900

    Fix: Git push 수정

--- app/git_manager.py
+++ app/git_manager.py
@@ -140,7 +140,7 @@ class GitManager:
             if self._is_empty_branch():
                 return False
             self._commit()
-            self._origin.push()
+            self._origin.push(f'--force')
             self.splog.info('GIT PUSH 성공')
             return True
         except Exception as e:
