b9a1657fe9b95eb03ab9830800952da98289ee4d
CGLee <cglee@snowpipe.co.kr>
2022-07-13 18:43:01 +0900

    Fix : DB동기화 방식 수정

--- app/prisma_manager.py
+++ app/prisma_manager.py
@@ -294,7 +294,8 @@ datasource db {{
 
     def _load_db_source(self):
         try:
-            os.chdir(self.PATH_FOR_SAVE_DIR)
+            from prisma_cleanup import cleanup
+            cleanup()
             data_source = SourceFileLoader(str(uuid.uuid4()), str(self.PATH_FOR_DATA_SOURCE)).load_module()
             self.data_db = data_source.Prisma()
         except Exception as e:
