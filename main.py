b87de06c88e4cd370b405926fe29dd46c9e5086b
CGLee <cglee@snowpipe.co.kr>
2022-08-19 14:49:47 +0900

    Fix: Excel Row추가시 DB Update되지않는 버그 수정

--- main.py
+++ main.py
@@ -84,17 +84,15 @@ async def task_excel_to_data_all_from_branch(br: str, modified: bool = False):
         if g_manager.is_modified():
             g_manager.push()
 
-            # 수정된 Json 파일이 있다면 Excel Git서버로 자동 커밋
-            if g_manager.is_modified():
-                g_manager.push()
-                g_manager.splog.info(f'EXCEL파일 데이터 수정으로 인한 데이터 업데이트를 진행합니다.')
-                await task_update_table(br)
-
             if g_manager.is_modified_excel_column():
                 g_manager.splog.add_info('기획 데이터의 컬럼에 변동 사항이 있습니다. 개발후 DB 마이그레이션을 진행 해 주세요.', 0)
                 g_manager.splog.send_developer_all()
                 g_manager.splog.send_designer('기획 데이터의 컬럼에 변동 사항이 있습니다. 개발자가 확인 후 다음 프로세스로 진행됩니다.')
-            g_manager.destroy()
+            else:
+                g_manager.splog.info(f'EXCEL파일 데이터 수정으로 인한 데이터 업데이트를 진행합니다.')
+                await task_update_table(br)
+
+        g_manager.destroy()
 
         excel_task.done()
 
